25e5d1ccf2b3efd9df9d4541c75c0023cbaa009f
Stoyan Stoyanov <stoqn.stoqnov.93@gmail.com>
2022-03-24 01:28:30 +0200

    added simple `ModalTransition` tests

---
+++ HedgehogLabDemoTests/User Interface/Routing/Transitions/ModalTransitionTests.swift
@@ -0,0 +1,40 @@
+//
+//  ModalTransitionTests.swift
+//  HedgehogLabDemoTests
+//
+//  Created by Stoyan Stoyanov on 24/03/22.
+//
+
+import XCTest
+@testable import HedgehogLabDemo
+
+class ModalTransitionTests: XCTestCase {
+
+    func testThatModalTransitionPresentsControllersWithCoverVerticalTransitionStyleAndCurrentContextPresentationStyle() throws {
+        // given
+        let sut = ModalTransition(isAnimated: true, modalTransitionStyle: .coverVertical, modalPresentationStyle: .currentContext)
+        let openedController = UIViewController(nibName: nil, bundle: nil)
+        let fromController = UIViewController(nibName: nil, bundle: nil)
+        
+        // when
+        sut.open(openedController, from: fromController, completion: nil)
+        
+        // then
+        XCTAssertEqual(openedController.modalPresentationStyle, .currentContext)
+        XCTAssertEqual(openedController.modalTransitionStyle, .coverVertical)
+    }
+    
+    func testThatModalTransitionPresentsControllersWithCrossDissolveTransitionStyleAndFormSheetPresentationStyle() throws {
+        // given
+        let sut = ModalTransition(isAnimated: true, modalTransitionStyle: .crossDissolve, modalPresentationStyle: .formSheet)
+        let openedController = UIViewController(nibName: nil, bundle: nil)
+        let fromController = UIViewController(nibName: nil, bundle: nil)
+        
+        // when
+        sut.open(openedController, from: fromController, completion: nil)
+        
+        // then
+        XCTAssertEqual(openedController.modalPresentationStyle, .formSheet)
+        XCTAssertEqual(openedController.modalTransitionStyle, .crossDissolve)
+    }
+}
