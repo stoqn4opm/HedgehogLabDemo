5edf00b08422477d2e18681b5e626dc695890a54
Stoyan Stoyanov <stoqn.stoqnov.93@gmail.com>
2022-03-29 16:37:40 +0300

    startup actions updated to pass the favorite photo service

--- Composition Root/Startup/Actions/LoadImgurPhotoService.swift
+++ Composition Root/Startup/Actions/LoadImgurPhotoService.swift
@@ -11,7 +11,7 @@ import NetworkingKit
 
 // MARK: - UIWindow Startup Action
 
-final class LoadImgurPhotoService: TransformerStartupAction<String, Result<(search: PhotoService, favorite: PhotoService), Error>> {
+final class LoadImgurPhotoService: TransformerStartupAction<String, Result<(search: PhotoService, favorite: PhotoService & PhotoServiceModifiable), Error>> {
     
     init() {
         super.init { appClientId in
@@ -20,20 +20,22 @@ final class LoadImgurPhotoService: TransformerStartupAction<String, Result<(sear
                                                                      fileManager: .default,
                                                                      maxAllowedDiskUsageInBytes: oneGigabyte),
                   
-                    let favoriteAccessor = CacheDirectoryRawDataAccessor(cacheSubdirectory: "Favorites",
+                    let favoriteAccessor = DestructiveCacheDirectoryRawDataAccessor(cacheSubdirectory: "Favorites",
                                                                          fileManager: .default,
                                                                          maxAllowedDiskUsageInBytes: oneGigabyte)
             else {
                 return .failure(NSError(domain: "[LoadImgurPhotoService] - no enough space on disk!", code: 1))
             }
             
-            return .success((search: Self.searchPhotoService(appClientId: appClientId, accessor: searchAccessor),
-                             favorite: Self.favoritePhotoService(accessor: favoriteAccessor)))
+            let searchPhotoService = Self.searchPhotoService(appClientId: appClientId, accessor: searchAccessor)
+            let favoritesPhotoService = Self.favoritePhotoService(sourcePhotoService: searchPhotoService, accessor: favoriteAccessor)
+            
+            return .success((search: searchPhotoService, favorite: favoritesPhotoService))
         }
     }
     
     @available(*, unavailable)
-    override init(input: String? = nil, transform: @escaping (String) -> (Result<(search: PhotoService, favorite: PhotoService), Error>)) {
+    override init(input: String? = nil, transform: @escaping (String) -> (Result<(search: PhotoService, favorite: PhotoService & PhotoServiceModifiable), Error>)) {
         super.init(input: input, transform: transform)
     }
 }
@@ -47,12 +49,12 @@ extension LoadImgurPhotoService {
         return CachingPhotoService(photoRepository: repository, photoStorage: photoStorage)
     }
     
-    private static func favoritePhotoService(accessor: RawDataHandler) -> PhotoService {
+    private static func favoritePhotoService(sourcePhotoService: PhotoService, accessor: RawDataHandler & RawDataDestructor) -> PhotoService & PhotoServiceModifiable {
         let repository = FavoritesPhotoRepository(rawDataHandler: accessor,
                                                   originalSizeMainRecordKey: "originalSizeRecords.json",
                                                   thumbnailSizeMainRecordKey: "thumbnailSizeRecords.json",
                                                   pageSize: 10)
         
-        return NonCachingPhotoService(photoRepository: repository, accessor: accessor)
+        return FavoritesPhotoService(sourcePhotoService: sourcePhotoService, photoRepository: repository, accessor: accessor)
     }
 }
